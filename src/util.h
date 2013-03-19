d185de03a185cbcebff934f82afc8ded69aa9a9c
yuyuyu101 <haomaiwang@gmail.com>
2013-03-19 16:23:45 +0800

    Change validators to config.c

--- src/util.h
+++ src/util.h
@@ -9,12 +9,6 @@ extern struct dictType sliceDictType;
 
 void nonBlockCloseOnExecPipe(int *fd0, int *fd1);
 
-/* Configuration Validator */
-int stringValidator(struct configuration *conf, const char *key, const char *val);
-int unsignedIntValidator(struct configuration *conf, const char *key, const char *val);
-int enumValidator(struct configuration *conf, const char *key, const char *val);
-int boolValidator(struct configuration *conf, const char *key, const char *val);
-
 int daemonize(int coredump);
 void createPidFile();
 void setProctitle(const char *title);
