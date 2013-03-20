ad69b9292193084450205a118c2c90a3f865c48f
yuyuyu101 <haomaiwang@gmail.com>
2013-03-21 00:22:10 +0800

    Add tests for protocol Redis

---
+++ tests/test_redis.py
@@ -0,0 +1,17 @@
+from wheatserver_test import WheatServer, PROJECT_PATH, server_socket
+import time
+import os
+import redis
+
+def test_redis():
+    async = WheatServer("", "--worker-type %s" % "AsyncWorker",
+                               "--app-project-path %s" % os.path.join(PROJECT_PATH, "example"),
+                               "--document-root %s" % PROJECT_PATH + "/example/",
+                               "--static-file-dir %s" % "/static",
+                               "--protocol Redis")
+    time.sleep(0.1)
+    r = redis.StrictRedis(port=10828)
+    for i in range(100):
+        assert r.set(str(i), i)
+    for i in range(100):
+        assert r.get(str(i)) == str(i)
