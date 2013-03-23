63de4700c9f73c1ba46530bbe5e4ddf3ff6e6aa2
yuyuyu101 <haomaiwang@gmail.com>
2013-03-24 01:30:30 +0800

    Add ll2string and intDictType

--- src/util.h
+++ src/util.h
@@ -6,6 +6,7 @@ struct dictType;
 struct configuration;
 extern struct dictType wstrDictType;
 extern struct dictType sliceDictType;
+extern struct dictType intDictType;
 
 void nonBlockCloseOnExecPipe(int *fd0, int *fd1);
 
@@ -15,6 +16,7 @@ void setProctitle(const char *title);
 int getFileSizeAndMtime(int fd, off_t *len, time_t *m_time);
 int isRegFile(const char *path);
 int fromSameParentDir(wstr left, wstr right);
+int ll2string(char *s, size_t len, long long value);
 
 void setTimer(int milliseconds);
 
