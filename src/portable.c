641f7bef1ee1d902b03c985470ea66ca26869791
yuyuyu101 <haomaiwang@gmail.com>
2013-03-25 14:26:34 +0800

    Fix portable_sendfile implement in osx platform

--- src/portable.c
+++ src/portable.c
@@ -5,12 +5,9 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 ssize_t portable_sendfile(int out_fd, int in_fd, off_t off, off_t len) {
-    ssize_t ret;
-    if ((ret = sendfile(in_fd, out_fd, off, &len, NULL, 0)) == -1) {
+    if (sendfile(in_fd, out_fd, off, &len, NULL, 0) == -1) {
         if (errno != EAGAIN)
             return -1;
-        else
-            return 0;
     }
     return len;
 }
