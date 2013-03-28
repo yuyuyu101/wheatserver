8da521517ed47c9227386494761f273f9ea2b2d3
yuyuyu101 <haomaiwang@gmail.com>
2013-03-28 22:07:16 +0800

    Upgrade version

--- src/version.h
+++ src/version.h
@@ -2,4 +2,4 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
-#define WHEATSERVER_VERSION "0.5.6"
+#define WHEATSERVER_VERSION "0.5.8"
