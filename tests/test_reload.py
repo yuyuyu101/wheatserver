ac2755a63cde7d6a697f3f80951ce32ccc0ba890
yuyuyu101 <haomaiwang@gmail.com>
2013-03-29 20:05:23 +0800

    Change test_reload.py

--- tests/test_reload.py
+++ tests/test_reload.py
@@ -6,10 +6,10 @@ import time
 async_server = None
 
 def test_reload():
-    async_server = WheatServer("", "--worker-type %s" % "AsyncWorker",
+    async_server = WheatServer("reload.conf", "--worker-type %s" % "AsyncWorker",
                                "--app-project-path %s" % os.path.join(PROJECT_PATH, "example"),
                                "--protocol Http")
-    time.sleep(0.5)
+    time.sleep(1)
     os.kill(async_server.exec_pid, signal.SIGHUP)
     s = server_socket(10829)
 
