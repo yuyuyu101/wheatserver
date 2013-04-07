68824d82e4488489b97d02756da7422c0cd2cde1
yuyuyu101 <haomaiwang@gmail.com>
2013-04-07 23:23:07 +0800

    Change header file

--- src/register.c
+++ src/register.c
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
-#include "worker/worker.h"
+#include "wheatserver.h"
 
 extern struct worker SyncWorker;
 extern struct worker AsyncWorker;
